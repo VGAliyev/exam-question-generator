7085686db981ffe42d31aa27a5a1b645dde0ebba
VGAliyev <v.aliev84@yandex.ru>
2023-12-30 15:25:08 +0400

    Init commit

---
+++ src/test/java/pro/sky/java/course2/valiyev/examquestiongenerator/ExamQuestionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.valiyev.examquestiongenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
